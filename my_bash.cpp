11613aa804c483dc3c2a5f6c472c39f09f6b5792
TalMalchi <malchital@gmail.com>
2022-03-16 15:03:02 +0200

    DIR

--- my_bash.cpp
+++ my_bash.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <stdlib.h>
+#include <dirent.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -10,14 +11,14 @@ using namespace std;
 int main(int argc, const char **argv)
 {
 
-    cout << "Yes Master ? " << endl;
-
-    //get current lib
+    // get current lib
     char cwd[1024];
     getcwd(cwd, sizeof(cwd));
     cout << cwd << ": " << endl;
 
-    //get input from the user
+    cout << "Yes Master ? " << endl;
+
+    // get input from the user
     string line, cmd, data;
     while (cmd != "EXIT")
     {
@@ -29,22 +30,48 @@ int main(int argc, const char **argv)
             // rest line is data from the user
             data = line.substr(line.find_first_of(" ") + 1);
         }
-        if(cmd == "ECHO"){
-            cout << data << endl; 
+        if (cmd == "ECHO")
+        {
+            cout << data << endl;
+        }
+        if (cmd == "TCP PORT")
+        {
+        }
+        if (cmd == "LOCAL")
+        {
+        }
+        if (cmd == "DIR")
+        {
+            DIR *dir_handler;
+            struct dirent *files;
 
-        }
-        if(cmd == "TCP PORT"){
+
+            dir_handler = opendir(".");
+            if (dir_handler)
+            {
+
+                while ((files = readdir(dir_handler)) != NULL){
+                    cout << files->d_name << endl;
             
-        }
-        if(cmd == "LOCAL"){
-            
-        }
-        if(cmd == "DIR"){
-            
-        }
-        if(cmd == "CD"){
-            
+            }
+            }
+
+            else {
+                cout << "Error" << endl; 
+            }  
+            closedir(dir_handler); 
+
+             
+
+    }
+    if (cmd == "CD")
+    {
+        if (data == ".." || data == "\n")
+        {
+            chdir("..");
+            //getenv
         }
     }
-    return 0; 
+}
+    return 0;
 }
