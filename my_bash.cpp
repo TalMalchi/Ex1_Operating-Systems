684b7f7b4b51e455c112e8fd112f5b7961d3030d
TalMalchi <malchital@gmail.com>
2022-03-16 17:36:21 +0200

    Update my_bash.cpp

--- my_bash.cpp
+++ my_bash.cpp
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <pthread.h>
+#include <cstdio>
 
 using namespace std;
 
@@ -73,31 +74,66 @@ int main(int argc, const char **argv)
         system("cd");
 
     }
+    //copy file from source to dest
+    if (cmd == "COPY")
+    {
+    //     string src, dst; 
+    //     char buf[BUFSIZ]; //default is 8192 bytes- one char at the time
+    //     size_t size; // BUFFER_SIZE = 4096;
+    //     src = data.substr(0, data.find_first_of(" "));
+    //     dst = data.substr(data.find_first_of(" ") + 1);
+
+    //     // open src file
+    //     FILE* src_open_file= fopen(src, "r");
+    //     if (src_open_file == NULL)
+    //     {
+    //         cout << "Error" << endl; 
+    //         exit(EXIT_FAILURE);
+    //     }
+        
+    //     FILE* dst_open_file= fopen(dst, "w");
+    //     if (dst_open_file == NULL)
+    //     {
+    //         fclose(src_open_file);
+    //         cout << "Error" << endl; 
+    //         exit(EXIT_FAILURE);
+    //     }
+
+    //     while (size = fread(buf, 1, BUFSIZ, src_open_file)) {
+    //         fwrite(buf, 1, size, dst_open_file);
+    // }
+
+    // fclose(src_open_file);
+    // fclose(dst_open_file);
+
+
+     }
+    
 }
     return 0;
 
     ////////////// fork /////////////
 
-    pid_t ch_pid = fork();
+    // pid_t ch_pid = fork();
 
-    // if pid == -1, fork faild
-    if (ch_pid == -1) {
-        perror("Fork failed");
-        exit(EXIT_FAILURE);
+    // //// if pid == -1, fork faild
+    // if (ch_pid == -1) {
+    //     perror("Fork failed");
+    //     exit(EXIT_FAILURE);
 
-    } 
-    ////////if pid >0 , I'm parent
-    else if (ch_pid > 0) {
-        cout << "spawn child with pid - " << ch_pid << endl;
-        return ch_pid;
-        }
+    // } 
+    // //////// if pid >0 , I'm parent
+    // else if (ch_pid > 0) {
+    //     cout << "spawn child with pid - " << ch_pid << endl;
+    //     return ch_pid;
+    //     }
 
-    ////////if pid ==0 , I'm child
-    else {
-        char* args[2]= {"./check", NULL}
-        execve(args[0], args, nullptr);
-        perror("execve");
-        exit(EXIT_FAILURE);
-    }
+    // //////// if pid ==0 , I'm child
+    // else {
+    //     char* args[2]= {"./check", NULL} //////////// need to do another file for this function 
+    //     execve(args[0], args, nullptr);
+    //     perror("execve");
+    //     exit(EXIT_FAILURE);
+    // }
 }
 
