47afd0b425e8fe9e7cf02bd5f009c30e3b85c345
TalMalchi <malchital@gmail.com>
2022-03-18 13:33:37 +0200

    COPY, DELETE, CD

--- my_bash.cpp
+++ my_bash.cpp
@@ -7,26 +7,34 @@
 #include <sys/types.h>
 #include <pthread.h>
 #include <cstdio>
+#include <sys/socket.h>
 
 using namespace std;
+
+#define PORT 8080
+
 
 int main(int argc, const char **argv)
 {
 
     // get current lib
     char cwd[1024];
-    getcwd(cwd, sizeof(cwd));
-    cout << cwd << ": " << endl;
+    
 
-    cout << "Yes Master ? " << endl;
 
     // get input from the user
     string line, cmd, data;
     while (cmd != "EXIT")
     {
+        // print current directory each time
+        getcwd(cwd, sizeof(cwd));
+        cout << cwd << ": " << endl;
+        cout << "Yes Master ? " << endl;
+
         getline(cin, line);
         // first word is cmd
         cmd = line.substr(0, line.find_first_of(" "));
+        
         if (cmd.size() < line.size())
         {
             // rest line is data from the user
@@ -42,13 +50,14 @@ int main(int argc, const char **argv)
 
         if (cmd == "TCP PORT")
         {
+
         }
 
         if (cmd == "LOCAL")
         {
         }
 
-        if (cmd == "DIR")
+        if (cmd == "DIR") /// check the different between file and directory
         {
             DIR *dir_handler;
             struct dirent *files;
@@ -83,13 +92,14 @@ int main(int argc, const char **argv)
         }
         else {
             //const char *new_cd= data.c_str(); 
-            string a = "/";
+            string a = "";
             a.append(data);
             const char *new_cd= a.c_str(); // convert string to const char 
             //cout <<a << endl;
             int check ; 
             cout <<new_cd << endl;
-            check = chdir(new_cd); //////////////check///////// if directory was change successfully, check=0 , else -1 
+            check = chdir(new_cd);// if directory was change successfully, check=0 , else -1 
+            cout << check << endl;
             cout << "after change" << endl;
 
 
@@ -97,40 +107,42 @@ int main(int argc, const char **argv)
         //system("cd");
 
     }
+
     //copy file from source to dest
-    // if (cmd == "COPY")
-    // {
-    //     string src, dst; 
-    //     char buf[BUFSIZ]; //default is 8192 bytes- one char at the time
-    //     size_t size; // BUFFER_SIZE = 4096;
-    //     src = data.substr(0, data.find_first_of(" "));
-    //     dst = data.substr(data.find_first_of(" ") + 1);
+    if (cmd == "COPY")
+    {
+        string src, dst; 
+        char buf[BUFSIZ]; //default is 8192 bytes- one char at the time
+        size_t size; // BUFFER_SIZE = 4096;
+        src = data.substr(0, data.find_first_of(" "));
+        dst = data.substr(data.find_first_of(" ") + 1);
 
-    //     // open src file
-    //     FILE* src_open_file= fopen(src, "r");
-    //     if (src_open_file == NULL)
-    //     {
-    //         cout << "Error" << endl; 
-    //         exit(EXIT_FAILURE);
-    //     }
-        
-    //     FILE* dst_open_file= fopen(dst, "w");
-    //     if (dst_open_file == NULL)
-    //     {
-    //         fclose(src_open_file);
-    //         cout << "Error" << endl; 
-    //         exit(EXIT_FAILURE);
-    //     }
+        // open src file
+        FILE* src_open_file= fopen(src.c_str(), "rb");
+        if (src_open_file == NULL)
+        {
+            cout << "Error" << endl; 
+            exit(EXIT_FAILURE);
+        }
 
-    //     while (size = fread(buf, 1, BUFSIZ, src_open_file)) {
-    //         fwrite(buf, 1, size, dst_open_file);
-    // }
+        // open dest file
+        FILE* dst_open_file= fopen(dst.c_str(), "wb");
+        if (dst_open_file == NULL)
+        {
+            fclose(src_open_file);
+            cout << "Error" << endl; 
+            exit(EXIT_FAILURE);
+        }
+        // write to the file 
+        while (size = fread(buf, 1, BUFSIZ, src_open_file)) {
+            fwrite(buf, 1, size, dst_open_file);
+    }
 
-    // fclose(src_open_file);
-    // fclose(dst_open_file);
+    fclose(src_open_file);
+    fclose(dst_open_file);
 
 
-    //  }
+    }
 
      if(cmd == "DELETE"){
         const char *file_name= data.c_str(); 
